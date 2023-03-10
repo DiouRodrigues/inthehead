4702e6b6a5199d0eaeb1588e209b80d7252ef4d6
Diovane Rodrigues <diovanerodriguees@outlook.com>
2023-03-10 18:14:57 -0300

    first commit

---
+++ content.md
@@ -0,0 +1,6 @@
+<ion-icon name="headset-sharp"></ion-icon> headphones
+<ion-icon name="menu"></ion-icon> menu
+<ion-icon name="cart-outline"></ion-icon> cart
+
+fontes
+font-family: 'Roboto', sans-serif; 100 300 400 700
---
+++ main.js
@@ -0,0 +1,19 @@
+let menu = document.querySelector("#menu");
+let nav = document.querySelector('.nav');
+
+
+
+
+
+
+menu.onclick = ()=>{
+    
+    nav.classList.toggle('active')
+}
+/* window.onscroll= () => {
+    if(window.scrollY > 55){
+        document.querySelector('#scrol' ).classList.add('active')
+    }else{document.querySelector('#scrol').classList.remove('active')}
+
+} */
+
